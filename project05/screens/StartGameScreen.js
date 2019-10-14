d93552de6626e26d79bfb553747051b4fe9bb49e
mike <mpakleung@gmail.com>
2019-10-14 14:21:22 -0400

    Style updates

--- project05/screens/StartGameScreen.js
+++ project05/screens/StartGameScreen.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import Card from '../components/Card';
 
 const StartGameScreen = props => {
 
     return (
         <View style={styles.screen}>
-            <Text>Start</Text>
+            <Text style={styles.title}>Start a new game</Text>
+            <Card style={styles.inputContainer}>
+                <View style={styles.inputContainer}>
+                    <Text>Select a number</Text>
+                    <TextInput />
+                    <View style={styles.buttonContainer}>
+                        <Button title="Reset" onPress={() => { }} />
+                        <Button title="Confirm" onPress={() => { }} />
+                    </View>
+                </View>
+            </Card>
         </View>
     )
 };
@@ -16,6 +26,22 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+    },
+
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
     }
 });
 
