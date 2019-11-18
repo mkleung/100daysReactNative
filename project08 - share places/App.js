585e2868d3f0b8ba222393b7c56c0aea43f17f87
mike <mpakleung@gmail.com>
2019-11-18 17:13:30 -0500

    add redux

--- project08 - share places/App.js
+++ project08 - share places/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
-
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
 import PlacesNavigator from './navigation/PlacesNavigator'
+import ReduxThunk from 'redux-thunk'
+
+import placesReducer from './store/places-reducer'
+
+const rootReducer = combineReducers({
+  places: placesReducer
+})
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk))
 
 export default function App() {
-  return <PlacesNavigator />
+  return (
+    <Provider store={store}>
+      <PlacesNavigator />
+    </Provider>
+  )
 }
 
--- project08 - share places/screens/PlacesListScreen.js
+++ project08 - share places/screens/PlacesListScreen.js
@@ -1,13 +1,28 @@
 import React from 'react'
-import { View, Text, StyleSheet, Platform } from 'react-native'
+import { View, Text, StyleSheet, Platform, FlatList } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import HeaderButton from '../components/HeaderButton'
+import { useSelector } from 'react-redux'
+import PlaceItem from '../components/PlaceItem'
 
 const PlacesListScreen = props => {
+
+    const places = useSelector(state => state.places.places)
+
     return (
-        <View>
-            <Text>Places List</Text>
-        </View>
+        <FlatList
+            data={places}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={(itemData, index) =>
+                <PlaceItem
+                    key={index}
+                    image={null}
+                    title={itemData.item.title}
+                    address={null}
+                    onSelect={() => {
+                        props.navigation.navigate("PlaceDetail",
+                            { placeTitle: itemData.item.title, placeId: itemData.item.id })
+                    }} />} />
     )
 }
 
