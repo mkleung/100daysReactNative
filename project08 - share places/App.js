c002911c9630d00ce938149f62109004d492f246
mike <mpakleung@gmail.com>
2019-11-20 12:38:39 -0500

    Setup SQLite for permanent data storage

--- project08 - share places/App.js
+++ project08 - share places/App.js
@@ -3,8 +3,17 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import PlacesNavigator from './navigation/PlacesNavigator'
 import ReduxThunk from 'redux-thunk'
-
+import { init } from './helpers/db'
 import placesReducer from './store/places-reducer'
+
+init()
+  .then(() => {
+    console.log("Initialize database")
+  })
+  .catch(err => {
+    console.log("Initializing db failed")
+    console.log(err)
+  })
 
 const rootReducer = combineReducers({
   places: placesReducer
