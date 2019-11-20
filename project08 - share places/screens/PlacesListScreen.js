ed2eca244879bb3b04763c95599ca95c8849aee7
mike <mpakleung@gmail.com>
2019-11-20 14:27:52 -0500

    Add fetch data from sql

--- project08 - share places/screens/PlacesListScreen.js
+++ project08 - share places/screens/PlacesListScreen.js
@@ -1,30 +1,40 @@
-import React from 'react'
-import { View, Text, StyleSheet, Platform, FlatList } from 'react-native'
-import { HeaderButtons, Item } from 'react-navigation-header-buttons'
-import HeaderButton from '../components/HeaderButton'
-import { useSelector } from 'react-redux'
-import PlaceItem from '../components/PlaceItem'
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, Platform, FlatList } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector, useDispatch } from 'react-redux';
+
+import HeaderButton from '../components/HeaderButton';
+import PlaceItem from '../components/PlaceItem';
+import * as placesActions from '../store/places-actions';
 
 const PlacesListScreen = props => {
+    const places = useSelector(state => state.places.places);
+    const dispatch = useDispatch();
 
-    const places = useSelector(state => state.places.places)
+    useEffect(() => {
+        dispatch(placesActions.loadPlaces());
+    }, [dispatch]);
 
     return (
         <FlatList
             data={places}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={(itemData, index) =>
+            keyExtractor={item => item.id}
+            renderItem={itemData => (
                 <PlaceItem
-                    key={index}
                     image={itemData.item.imageUri}
                     title={itemData.item.title}
                     address={null}
                     onSelect={() => {
-                        props.navigation.navigate("PlaceDetail",
-                            { placeTitle: itemData.item.title, placeId: itemData.item.id })
-                    }} />} />
-    )
-}
+                        props.navigation.navigate('PlaceDetail', {
+                            placeTitle: itemData.item.title,
+                            placeId: itemData.item.id
+                        });
+                    }}
+                />
+            )}
+        />
+    );
+};
 
 PlacesListScreen.navigationOptions = navData => {
     return {
@@ -35,15 +45,14 @@ PlacesListScreen.navigationOptions = navData => {
                     title="Add Place"
                     iconName={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
                     onPress={() => {
-                        navData.navigation.navigate("NewPlace")
+                        navData.navigation.navigate('NewPlace');
                     }}
                 />
             </HeaderButtons>
         )
-    }
-}
+    };
+};
 
+const styles = StyleSheet.create({});
 
-const styles = StyleSheet.create({})
-
-export default PlacesListScreen
+export default PlacesListScreen;
